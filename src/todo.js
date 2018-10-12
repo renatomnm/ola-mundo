8e4b66c7f01fb96a626901add1c79f4c3efb8585
unknown <andres@digitalhouse.com>
2018-10-11 22:07:37 -0300

    Finalizando o exercicio de react

--- src/todo.js
+++ src/todo.js
@@ -6,22 +6,26 @@ import TodoList_input from './todolist_input'
 class TodoApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { items: [], text: '' };
+    this.state = { items: [], text: '', loading:true };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:8000/api/listTasks').then(response=>response.json()).then(has=>this.setState({items:has,loading:false}))
   }
 
   render() {
     return (
       <Fragment>
         <h3>TODO</h3>
-        <TodoList items={this.state.items} />
+        {this.state.loading ? "Aguarde um momento" : <TodoList items={this.state.items}/>}
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="new-todo">
             What needs to be done?
           </label>
           {/*<input id="new-todo" onChange={this.handleChange} value={this.state.text}/>*/}
-          <TodoList_input id="new-todo" onChange={this.handleChange}/>
+          <TodoList_input onChange={this.handleChange}/>
           <Botao items={this.state.items}/>
         </form>
       </Fragment>
@@ -34,17 +38,33 @@ class TodoApp extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
+
+    this.setState({loading:true})
+
     if (!this.state.text.length) {
       return;
     }
-    const newItem = {
+
+     const newItem = {
       text: this.state.text,
-      id: Date.now()
-    };
-    this.setState(state => ({
-      items: state.items.concat(newItem),
-      text: ''
-    }));
+       id: Date.now()
+     };
+
+    fetch('http://localhost:8000/api/addTask/',{
+    	method:'POST',
+    	headers: new Headers({'content-type': 'application/json'}),
+    	body: JSON.stringify({
+    	  "tarefa": this.state.text
+    	}),
+    })
+    .then(res => res.json())
+    .then(has => this.setState(state => ({
+          items: state.items.concat(newItem),
+          text: '',
+          loading:false
+        })
+      )
+    )
   }
 }
 
